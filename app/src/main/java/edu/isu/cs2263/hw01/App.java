1fc6b82b66e8008e5d57219a69f6311b7e78f7ba
David Hellwig <helldavi@isu.edu>
2021-09-09 20:48:55 -0600

    Last commit didn't work, this is a test

--- app/src/main/java/edu/isu/cs2263/hw01/App.java
+++ app/src/main/java/edu/isu/cs2263/hw01/App.java
@@ -6,10 +6,15 @@ package edu.isu.cs2263.hw01;
 
 import com.sun.jdi.connect.Connector;
 import org.apache.commons.cli.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import java.util.Arrays;
 
 public class App {
+
+
 
 
 
@@ -26,35 +31,42 @@ public class App {
         options.addOption("o","output",true,"sends the output to the provided file");
 
         CommandLineParser parser = new DefaultParser();
+        try{
+            CommandLine cmd = parser.parse(options, args);
 
-        CommandLine cmd = parser.parse(options, args);
+            if(cmd.hasOption("h")) {
+                System.out.println("""
+                        usage:eval [Options]
+                        Evaluation of mathemematical expressions
+                        -b,--batch <file>    batch file containing expressions to evaluate
+                        -h,--help            print usage message
+                        -o,--output <file>   output file
+                        \t\tCopyright (C) 2021 Isaac D. Griffith
+                        """);
+                System.exit(0);
+            }
+            else if (cmd.hasOption("b")){
+                String temp = Arrays.toString(cmd.getArgs());
+                System.out.println("batch:".concat(temp));
 
-        if(cmd.hasOption("h")) {
-            System.out.println("""
-                    usage:eval [Options]
-                    Evaluation of mathemematical expressions
-                    -b,--batch <file>    batch file containing expressions to evaluate
-                    -h,--help            print usage message
-                    -o,--output <file>   output file
-                    \t\tCopyright (C) 2021 Isaac D. Griffith
-                    """);
-            System.exit(0);
-        }
-        else if (cmd.hasOption("b")){
-            String temp = Arrays.toString(cmd.getArgs());
-            System.out.println("batch:".concat(temp));
-
-        }
-        else if (cmd.hasOption("o")){
-            String temp = Arrays.toString(cmd.getArgs());
-            System.out.println("batch:".concat(temp));
-
-        }
-
-
+            }
+            else if (cmd.hasOption("o")){
+                String temp = Arrays.toString(cmd.getArgs());
+                System.out.println("output:".concat(temp));
 
         }
-        
+
+
+
+        }
+        catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
 
 
     }
