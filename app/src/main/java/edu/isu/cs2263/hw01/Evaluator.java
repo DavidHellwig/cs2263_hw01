d71fe3dc36dadf632b0059637a3f00ef106d2469
David Hellwig <helldavi@isu.edu>
2021-09-15 20:43:03 -0600

    Fixed issue where evaluate was stuck in loop evaluate still properly accessing memory

--- app/src/main/java/edu/isu/cs2263/hw01/Evaluator.java
+++ app/src/main/java/edu/isu/cs2263/hw01/Evaluator.java
@@ -1,10 +1,10 @@
 //Author:David Hellwig
 
 package edu.isu.cs2263.hw01;
-import javax.annotation.Nullable;
+
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 
@@ -60,66 +60,69 @@ public class Evaluator {
 
     }
     //Just print results of expression
-    public void showResults(ArrayList<Integer> a){
-        System.out.println(a.get(0));
+    public void showResults(int a){
+        System.out.println(a);
     }
     //Evaluate expression
     public void Evaluate(){
-        int initialCapacity = 1000;
-
-        //ArrayList<String> inputMemory = new ArrayList<>(initialCapacity);
-
-        ArrayList<Integer> Memory = new ArrayList<>(2);
-
 
         Scanner scanner = new Scanner(System.in);
 
-        String stringBreaker = scanner.nextLine();
+        String[] stringBreaker = scanner.nextLine().split("[+/*-]");
 
-        ArrayList<String> inputMemory = new ArrayList<>(Arrays.asList(stringBreaker.split("[/*+-]")));
+        int i = 0;
 
+        int j = stringBreaker.length;
 
-        Memory.add(Integer.parseInt(inputMemory.remove(0)));
+        int Memory = 0;
 
         //Read symbol by symbol to figure out what to do next. Does not consider order of operations
-        do {
+        while (i < j){
 
-            //checkForDoubleOperator(inputMemory);
-            if (inputMemory.get(0).equals("/")){
-                Memory.add(divideNumber(Memory.remove(0), Integer.parseInt(inputMemory.remove(1))));
-                inputMemory.remove(0);
-                inputMemory.remove(1);
+            try {
+                //checkForDoubleOperator(inputMemory);
+                if (stringBreaker[i + 1].equals("/")) {
 
-                break;
+                    Memory += divideNumber(Integer.parseInt(stringBreaker[i]), Integer.parseInt(stringBreaker[i + 2]));
+                    i = i + 3;
+
+                    break;
+
+
+                } else if (stringBreaker[i + 1].equals("+")) {
+                    Memory += addNumbers(Integer.parseInt(stringBreaker[i]), Integer.parseInt(stringBreaker[i + 2]));
+
+                    i = i + 3;
+
+                    break;
+                } else if (stringBreaker[i + 1].equals("-")) {
+                    Memory += subtractNumbers(Integer.parseInt(stringBreaker[i]), Integer.parseInt(stringBreaker[i + 2]));
+                    i = i + 3;
+
+                    break;
+                } else if (stringBreaker[i + 1].equals("*")) {
+                    Memory =+ multiplyNumber(Integer.parseInt(stringBreaker[i]), Integer.parseInt(stringBreaker[i + 2]));
+                    i = i + 3;
+
+                    break;
+                } else {
+                    i += 1;
+                }
+            }
+            catch (IndexOutOfBoundsException e){
+                showResults(Memory);
+                System.exit(0);
+
+
 
             }
-            else if (inputMemory.get(0).equals("+")){
-                Memory.add(addNumbers(Memory.remove(0), Integer.parseInt(inputMemory.remove(1))));
-                inputMemory.remove(0);
-                inputMemory.remove(1);
-
-                break;
-            }
-            else if (inputMemory.get(0).equals("-")){
-                Memory.add(subtractNumbers(Memory.remove(0), Integer.parseInt(inputMemory.remove(1))));
-                inputMemory.remove(0);
-                inputMemory.remove(1);
-
-                break;
-            }
-            else if (inputMemory.get(0).equals("*")){
-                Memory.add(multiplyNumber(Memory.remove(0), Integer.parseInt(inputMemory.remove(1))));
-                inputMemory.remove(0);
-                inputMemory.remove(1);
-
-                break;
-            }
 
 
 
 
-        }while (inputMemory.size() != 0 );
-        showResults(Memory);
+
+        }
+
 
 
     }
